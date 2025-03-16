919427d04f59919325d7081daa69e66c8b6748d2
Diego-Henrique <diegohenrique12200@gmail.com>
2025-03-16 14:55:09 -0300

    Carrinho de Compras

---
+++ carrinho.js
@@ -0,0 +1,40 @@
+var qtd = [0,0,0];                          //quantidade de cada produto
+var valorProduto = [45.00,39.90, 49.90];    //valor unitário de cada produto
+var valorTotal = [0,0,0];                   //valor total de cada produto
+
+function adicionarItem(item){
+    var quantidade = document.getElementById('quantidade' + item);
+    qtd[item] = qtd[item] + 1; //cálculo que adiciona +1 na quantidade
+    quantidade.innerHTML = qtd[item]; //exibe o valor da quantidade na tela
+   
+    var total = document.getElementById("total" + item);
+    valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item]; 
+    //multiplica o valor unitário pela quantidade 
+    total.innerHTML =  valorTotal[item].toFixed(2); //exibe o valor total do produto na tela
+
+    valorCompra();
+}
+function removerItem(item){
+    if(qtd[item] > 0){
+        var quantidade = document.getElementById('quantidade' + item);
+        qtd[item] = qtd[item] - 1; //cálculo que remove -1 na quantidade
+        quantidade.innerHTML = qtd[item]; //exibe o valor da quantidade na tela
+
+        var total = document.getElementById("total" + item);
+        valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+        //multiplica o valor unitário pela quantidade 
+        total.innerHTML =  valorTotal[item].toFixed(2);//exibe o valor total do produto na tela
+
+        valorCompra();
+    }
+}
+
+
+function valorCompra(){
+    var valorTotalCompra = document.getElementById('valorTotalCompra');
+    var valor = 0;
+    for(var i=0; i<valorTotal.length;i++){
+        valor+=valorTotal[i];
+    }
+    valorTotalCompra.innerHTML = valor.toFixed(2);
+}
